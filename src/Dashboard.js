2ce1ac89a6be6eb7d7155f597e5de9d0b22fe178
sneh shah <snehshah7634@gmail.com>
2024-02-26 19:47:59 +0530

    .env file added

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -18,12 +18,12 @@ const Dashboard = () => {
   const navigate = useNavigate(); // Change 'history' to 'navigate'
 
   const firebaseConfig = {
-    apiKey: "AIzaSyBaFxFtY1MUEVywG0GEjEflxa4L4PkE4qA",
-    authDomain: "linkersdb-firestore.firebaseapp.com",
-    projectId: "linkersdb-firestore",
-    storageBucket: "linkersdb-firestore.appspot.com",
-    messagingSenderId: "948306998721",
-    appId: "1:948306998721:web:001b9c324a55eb77dcab40"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
